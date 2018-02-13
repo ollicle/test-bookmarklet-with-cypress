80e1b9b757a88136f2e33bee6e0cb1f431c9a9e5
Oliver Boermans <ollie@ollicle.com>
2018-02-13 21:19:32 +1030

    Change port and try cy in place of Cypress.$

--- cypress/integration/inject-bookmarklet.js
+++ cypress/integration/inject-bookmarklet.js
@@ -1,9 +1,9 @@
 describe('Test a bookmarklet', function() {
   it('Inject the bookmarklet script into the test page', function() {
-	cy.visit('http://localhost:3333/');
+	cy.visit('http://localhost:3000/');
 	
 	//	I expected this script to be appended to the test page context
 	//	Instead it appears to be appended to the top level
-	Cypress.$('<script src="http://localhost:3333/bookmarklet.js"></script>').appendTo("head");
+	cy.get('head').then(($head) => { $head.append('<script src="http://localhost:3000/bookmarklet.js"></script>') })
   })
 })
