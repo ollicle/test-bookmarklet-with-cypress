55b17fb2869e0c5379048bb5596a83521b742c65
Oliver Boermans <ollie@ollicle.com>
2018-02-13 21:21:17 +1030

    Comment iframer code – demonstrate document context

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,11 @@
-//	Wipe out document
+console.log("Expected test document title: %cHello Cypress", 'font-weight:bold');
+console.log("Actual document title: %c" + document.title, 'font-weight:bold');
 
-//	Reload page into an iframe
+//	Wipe out document –	Reload page into an iframe
+/*
 document.body.innerHTML = '';
 
 const iframe = document.createElement('iframe');
 iframe.setAttribute('src', '?');
 document.body.append(iframe);
+*/
